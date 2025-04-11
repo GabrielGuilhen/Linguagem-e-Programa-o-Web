0917aa3acff89af3f26247da33dc48e66456a6a4
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2025-04-11 15:36:48 -0300

    Create post.php
    usar o thunder client para puxar as informações

---
+++ 11-04/tarefas da sala/post.php
@@ -0,0 +1,17 @@
+<?php
+
+//echo "teste!";
+//$numero1 = $_POST['numero1'];
+//echo "<br>";
+//$numero2 = $_POST['numero2'];
+//$soma= $numero1+$numero2;
+//echo "A soma dos dois números é:<br>" . $soma;
+
+
+//$n1 = $_POST ['n1'];
+//$n2 = $_POST ['n2'];
+//$n3 = $_POST     ['n3'];
+
+//$soma = $n1+$n2+$n3;
+//$media = $soma / 3;
+//echo "Resultado da Média = " . $media;
