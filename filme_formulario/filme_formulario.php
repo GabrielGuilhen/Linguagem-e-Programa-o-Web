ea91ee990dff54d8db3f660fa93bed34e3333664
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2025-05-09 14:32:39 -0300

    Create filme_formulario.php

---
+++ filme_formulario/filme_formulario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="filme_card.php" method="post">
+        <input type="text" name="nomeFilme" placeholder="Nome do Filme">
+        <input type="text" name="diretor" placeholder="diretor">
+        <input type="text" name="ano" placeholder="Ano">
+        <input type="text" name="foto" placeholder="Foto">
+
+
+        <button>Enviar</button>
+
+
+
+    </form>
+
+</body>
+
+</html>
