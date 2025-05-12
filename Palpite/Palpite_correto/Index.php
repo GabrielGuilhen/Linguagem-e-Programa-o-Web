30509c68542a0760fbafda136f362d445b0eef2c
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2025-05-11 22:56:25 -0300

    Update Index.php

--- Palpite/Palpite_correto/Index.php
+++ Palpite/Palpite_correto/Index.php
@@ -1,6 +1,6 @@
 <?php
-
-class ItemBatman {
+class ItemBatman
+{
     public function __construct(
         public $nome,
         public $imagem,
@@ -10,21 +10,9 @@ class ItemBatman {
 
 
 $itens = [
-    1 => new ItemBatman(
-        "Batarangue",
-        "img/Batarang.jpeg",
-        "Uma arma afiada que voa de volta ao dono!"
-    ),
-    2 => new ItemBatman(
-        "Cinto de Utilidades",
-        "img/Cinto_batman.jpeg",
-        "Contém ferramentas para qualquer situação!"
-    ),
-    3 => new ItemBatman(
-        "Bat-Sinal",
-        "img/Bat_sinal.jpeg",
-        "Ilumina os céus de Gotham!"
-    )
+    1 => new ItemBatman("Batarangue", "img/Batarang.jpeg", "Arma que voa de volta"),
+    2 => new ItemBatman("Cinto de Utilidades", "img/Cinto_batman.jpeg", "Ferramentas variadas"),
+    3 => new ItemBatman("Bat-Sinal", "img/Bat_sinal.jpeg", "Ilumina Gotham")
 ];
 
 
@@ -33,45 +21,45 @@ if (!isset($_SESSION['itemCorreto'])) {
     $_SESSION['itemCorreto'] = array_rand($itens);
 }
 $itemCorreto = $itens[$_SESSION['itemCorreto']];
-
-
 ?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Jogo da Adivinhação do Batman</title>
+    <title>Adivinhe o Item do Batman</title>
 </head>
+
 <body>
-    <h1>Jogo da Adivinhação do Batman</h1>
-    
-    <?php if (!isset($_GET['palpite'])): ?>
+    <h1>Adivinhe o Item do Batman</h1>
+
+    <?php if (!isset($_GET['palpite'])) { ?>
         <p>Escolha um palpite:</p>
         <ul>
-            <li><a href="?palpite=1">Batarangue</a></li>
-            <li><a href="?palpite=2">Cinto de Utilidades</a></li>
-            <li><a href="?palpite=3">Bat-Sinal</a></li>
+            <li><a href="?palpite=1">Item 1 - Batarangue</a></li>
+            <li><a href="?palpite=2">Item 2 - Cinto</a></li>
+            <li><a href="?palpite=3">Item 3 - Bat-Sinal</a></li>
         </ul>
-    <?php else: 
+    <?php } else {
         $palpite = (int)$_GET['palpite'];
-        
+
         if ($palpite < 1 || $palpite > 3) {
-            echo "<p>Erro: Palpite inválido! Use 1, 2 ou 3.</p>";
+            echo "<p>Erro: Use 1, 2 ou 3.</p>";
         } else {
             $itemSelecionado = $itens[$palpite];
-            
+
             if ($itemSelecionado === $itemCorreto) {
-                echo "<h2>Parabéns! Você acertou!</h2>";
+                echo "<h2>Acertou!</h2>";
                 echo "<img src='{$itemCorreto->imagem}' width='300'>";
-                echo "<p>{$itemCorreto->nome}: {$itemCorreto->dica}</p>";
                 unset($_SESSION['itemCorreto']);
             } else {
-                echo "<h2>Você errou!</h2>";
+                echo "<h2>Errou!</h2>";
                 echo "<p>Dica: {$itemCorreto->dica}</p>";
-                echo "<img src='{$itemCorreto->imagem}' width='300' style='filter: blur(5px)'>";
             }
         }
-    endif; ?>
-    
-    <p><a href="?">Jogar novamente</a></p>
+    } ?>
+
+    <p><a href="?">Tentar novamente</a></p>
 </body>
+
 </html>
