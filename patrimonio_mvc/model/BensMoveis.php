7fe7968172c70dffa701af08746bd6ebb371b201
Gabriel_Guilhen <gabrielguilhen0@gmail.com>
2025-09-25 23:00:36 -0300

    Create BensMoveis.php

---
+++ patrimonio_mvc/model/BensMoveis.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once(__DIR__ . "/../config.php");
+
+//Arquivo BensMoveis.php em model
+class BensMoveis {
+
+    public $id;
+    public $nome_da_escola;
+    public $itens;
+    public $marca;
+    public $estado_do_bem;
+    public $data_de_aquisicao;
+    public $id_bem_imovel;
+    public $justificativa;
+    public $data_transferencia;
+    
+}
